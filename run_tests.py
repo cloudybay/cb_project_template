39556f38ec8eabfaba43322906e5ac4c3688e79e
sunaley <sunaley@gmail.com>
2020-03-11 13:43:13 +0800

    add run_tests.py

---
+++ run_tests.py
@@ -0,0 +1,30 @@
+
+import os
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+import env
+
+from django.test.runner import DiscoverRunner
+
+
+class NoDbTestRunner(DiscoverRunner):
+    """ A test runner to test without database creation """
+
+    def setup_databases(self, **kwargs):
+        """ Override the database creation defined in parent class """
+        pass
+
+    def teardown_databases(self, old_config, **kwargs):
+        """ Override the database teardown defined in parent class """
+        pass
+
+
+if __name__ == "__main__":
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(keepdb=True)
+    failures = test_runner.run_tests(["{{ project_name }}"])
+    sys.exit(bool(failures))
