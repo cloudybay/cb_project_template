22df16efcbb911412b6037504b541893fe106d77
lanwei <lanwei@cloudybay.com.tw>
2020-08-31 14:13:16 +0800

    using path instead of urls

--- project_name/urls.py
+++ project_name/urls.py
@@ -5,15 +5,16 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+    2. Add a URL to urlpatterns:  path('blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.urls import path
+
 
 urlpatterns = [
 ]
