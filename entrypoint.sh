d6906a2ac8da80b1c7001fafc9a8262948ffb83c
sunaley <sunaley@gmail.com>
2023-08-02 09:38:42 +0000

    // fix gunicorn bind port

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 gunicorn \
     --preload \
-    --bind 0.0.0.0:8080 {{ project_name }}.wsgi \
+    --bind 0.0.0.0:8000 {{ project_name }}.wsgi \
     --timeout 360 --workers 5 --max-requests 10000 \
     --pid /var/run/app/gunicorn.pid
