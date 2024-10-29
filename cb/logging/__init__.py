f614e7bf5d2586d9d0e3556fbbe20af4f0b13a7c
sunaley <sunaley@gmail.com>
2024-10-29 09:35:26 +0800

    refactor: Update logging configuration and middleware

--- cb/logging/__init__.py
+++ cb/logging/__init__.py
@@ -20,12 +20,15 @@ class JsonFormatter(logging.Formatter):
             'path': record.pathname,
             'process': record.process,
             'message': record.getMessage(),
+            'username': record.username if hasattr(record, 'username') else None,
+            'uid': record.uid if hasattr(record, 'uid') else None,
+            'gid': record.gid if hasattr(record, 'gid') else None,
         }
         if record.exc_info:
             log_record['exception'] = ''.join(traceback.format_exception(*record.exc_info))
         return json.dumps(log_record, ensure_ascii=False)
 
-    def formatTime(self, record ):
+    def formatTime(self, record):
         dt = datetime.fromtimestamp(record.created, self.tz)
         if self.datefmt:
             return dt.strftime(self.datefmt)
@@ -46,65 +49,53 @@ class TimeZoneFormatter(logging.Formatter):
             return dt.isoformat()
 
 
+class UserInfoFilter(logging.Filter):
+    def filter(self, record):
+        record.uid = os.getuid()
+        record.gid = os.getgid()
+        return True
+
+
 LOGGING_CONF = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
         'json': {
-            '()': 'setup_logging.JsonFormatter',
+            '()': JsonFormatter,
             'datefmt': '%Y-%m-%dT%H:%M:%S.%f%z',
             'tz': pytz.timezone('Asia/Taipei')
         },
+        'verbose': {
+            '()': TimeZoneFormatter,
+            'format': '[%(asctime)s %(pathname)s:%(lineno)d (%(process)d) %(levelname)s] %(message)s',
+            'tz': pytz.timezone('Asia/Taipei')
+        }
     },
     'filters': {
+        'user_info': {
+            '()': UserInfoFilter,
+        }
     },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
             'formatter': 'json',
         },
+        'file': {
+            'class': 'logging.FileHandler',
+            'formatter': 'verbose',
+            'filename': os.path.join(os.environ['BASE_DIR'], 'logfiles', 'verbose.log'),
+        }
     },
     'loggers': {
         'cb': {
             'level': 'INFO',
-            'handlers': ['console'],
+            'handlers': ['console', 'file'],
+            'filters': ['user_info'],
             'propagate': False,
         }
     }
 }
 
-
-class ConfigError(Exception):
-    def __init__(self, levels, msg):
-        super().__init__(f"{'/'.join(levels)}: {msg}")
-
-
-def load(config_file):
-    conf_path = os.path.join(os.environ['BASE_DIR'], 'conf')
-    if not os.path.isabs(config_file):
-        operation_mode = os.environ['OPERATION_MODE']
-        potential_path = os.path.join(conf_path, operation_mode, config_file)
-        if os.path.exists(potential_path):
-            config_file = potential_path
-        else:
-            config_file = os.path.join(conf_path, config_file)
-
-    config = {}
-    try:
-        with open(config_file) as f:
-            exec(f.read(), {}, config)
-        return config
-    except Exception as e:
-        raise ConfigError(['ExecError'], f"Error during importing config: {config_file}") from e
-
-partial_conf = load('logging.conf')
-
-
-for key in LOGGING_CONF:
-    if conf := partial_conf.get(key):
-        LOGGING_CONF[key].update(conf)
-
-
 logging.config.dictConfig(LOGGING_CONF)
-
 logger = logging.getLogger('cb')
