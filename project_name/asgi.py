6827bb62a37b141f1bc99d97f172af922348a475
LANWEI SU <lanwei@cloudybay.com.tw>
2025-04-10 17:19:24 +0800

    Update asgi.py
    add ASGIStaticFilesHandler

--- project_name/asgi.py
+++ project_name/asgi.py
@@ -4,12 +4,14 @@ ASGI config for bbb project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/asgi/
 """
-
-from django.core.asgi import get_asgi_application
 
 from dotenv import load_dotenv
 load_dotenv()
 
-application = get_asgi_application()
+from django.core.asgi import get_asgi_application
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
+
+django_asgi_app = get_asgi_application()
+application = ASGIStaticFilesHandler(django_asgi_app)
