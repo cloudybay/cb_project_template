eda3f4af17891efeb7e24ac8f4f28a865ce3c446
sunaley <sunaley@gmail.com>
2025-04-11 09:37:08 +0800

    fix: Add CSRF middleware to settings

--- project_name/settings/settings.py
+++ project_name/settings/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
